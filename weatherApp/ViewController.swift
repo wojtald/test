941f48b13bb8a9fe95edb6dda5162255cbb56b95
Damian Wojtal <damian.wojtal@ts.fujitsu.com>
2021-06-30 13:25:06 +0200

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Damian on 30/06/2021.
+//  Copyright © 2021 Damian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
